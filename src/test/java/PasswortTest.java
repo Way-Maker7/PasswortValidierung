ce3a1bb771eb21a7b2ba8cfe6da992b7e2fba4b6
Denis Sabou <Denis_Sabou@gmx.de>
2022-05-31 09:11:06 +0200

    erste Version

---
+++ src/test/java/PasswortTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswortTest {
+
+    @Test
+     void shouldbeTrue(){
+
+    }
+
+    @Test
+    void shouldbeFalse(){
+
+    }
+
+    @Test
+    void shouldNotExit(){
+
+    }
+
+
+}
